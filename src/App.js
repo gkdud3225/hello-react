afb162d0978c7bc7ee0fc99c651ec0535c697e02
hayeong <gkdud3225@hanmail.net>
2018-06-28 16:36:54 +0900

    MyName 컴포넌트 생성. props 설정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import MyName from './MyName';
 
 class App extends Component { // 클래스 형태로 만들어진 컴포넌트에는 render함수가 필요하다.
   render() {
 
     return ( // 내부에서는 JSX를 리턴
-      <div className="App">
-        hello React!
-      </div>
+      <MyName />
     );
   }
 }
