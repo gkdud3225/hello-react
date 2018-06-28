ed42af9cfb13b1728cb8d9aa781264830af78106
hayeong <gkdud3225@hanmail.net>
2018-06-28 16:20:30 +0900

    css파일로 적용

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component { // 클래스 형태로 만들어진 컴포넌트에는 render함수가 필요하다.
   render() {
-    const style = {
-      backgroundColor: 'black',
-      color: 'white',
-      fontSize: '15px',
-      padding: '20px'
-    };
 
     return ( // 내부에서는 JSX를 리턴
-      <div style={style}>
+      <div className="App">
         hello React!
       </div>
     );
