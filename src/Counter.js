62f8e9fd971092a9ef5c7abb1804ceeb27e75671
hayeong <gkdud3225@hanmail.net>
2018-06-29 16:27:16 +0900

    componentDidCatch를 통한 에러 처리

--- src/Counter.js
+++ src/Counter.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 
+const Problematic = () => {
+        throw (new Error('버그'));
+        return (
+            <div>
+            </div>
+        );
+};
+
 class Counter extends Component {
     state = {
-        number: 0
+        number: 0,
+        error: false
     }
 
     // 또다른 state 정의 방법
@@ -55,11 +64,19 @@ class Counter extends Component {
         );
     }
 
+    componentDidCatch(error, info){
+        this.setState({
+            error: true
+        });
+    }
+
     render() {
+        if(this.state.error) return (<h1>에러발생!</h1>);
         return(
             <div> 
                 <h1>카운터</h1>
                 <div>값: {this.state.number}</div>
+                { this.state.number === 4 && <Problematic /> }
                 {/* React는 JS와 달리 이벤트에 전달해주는 값이 함수여야 한다! 사용자가 만든 메소드를 호출하면 무한반복이 되버린다! */}
                 <button onClick = {this.handleIncrease}> + </button>
                 <button onClick = {this.handleDecrease}> - </button>
